42685b22d7a97d4749dd9259a9f69b7d5d5fdf3c
valdemar.ceccon <valdemar.ceccon@gmail.com>
2017-10-30 21:55:50 -0200

    Adicionado abas para agrupar os status, adicionado evento no cards para mover entre os status, e adicioando barra de progresso para indicar os finalizados. Adicionado titulo no meu principal.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react';
-import { Menu, Container, Icon } from 'semantic-ui-react'
-import { Switch, Route, Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Menu, Container, Icon} from 'semantic-ui-react'
+import {Switch, Route, Link} from 'react-router-dom';
 import Home from './Home';
 import BooksProgress from './containers/bookProgress';
 import './App.css';
 
 const menuItens = [
-  {title: 'Books', icon: 'book', link: '/books'},
-  {title: 'Requests', icon: 'home'},
-  {title: 'Menu 3', icon: 'home'}
+    {title: 'Books', icon: 'book', link: '/books'},
+    {title: 'Requests', icon: 'home'},
+    {title: 'Menu 3', icon: 'home'}
 ];
 
 class App extends Component {
-  render() {
-    return (
-      <div >
-        <Menu fixed="top" inverted>
-          <Container>
-            {menuItens.map((i, index) => (
-              <Menu.Item as={Link} header key={index} to={i.link ? i.link: '/'}>
-                <Icon name={i.icon}/>
-                {i.title}
-              </Menu.Item>
-            ))}
-          </Container>
-        </Menu>
-        <Container style={{marginTop: '50px'}}>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/books' component={BooksProgress}/>
-          </Switch>
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Menu fixed="top" inverted>
+                    <Container>
+                        <Menu.Item header>Person 5 companion</Menu.Item>
+                        {menuItens.map((i, index) => (
+                            <Menu.Item as={Link} header key={index} to={i.link ? i.link : '/'}>
+                                <Icon name={i.icon}/>
+                                {i.title}
+                            </Menu.Item>
+                        ))}
+                    </Container>
+                </Menu>
+                <Container style={{marginTop: '50px'}}>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route exact path='/books' component={BooksProgress}/>
+                    </Switch>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
